30c252b5cf60cbf12c7a1f172a2bcab346a94eec
SOUMYA <SOUMYA@Soumya>
2018-10-05 09:19:30 +0530

    Test project committed

---
+++ src/DemoA.java
@@ -0,0 +1,10 @@
+
+public class DemoA {
+
+	public static void main(String[] args) {
+		System.out.println("New java project projectA2B");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/qsp/DemoA.java
@@ -0,0 +1,11 @@
+package qsp;
+
+public class DemoA {
+
+	public static void main(String[] args) {
+		System.out.println("New java project projectA2B");
+		// TODO Auto-generated method stub
+
+	}
+
+}
